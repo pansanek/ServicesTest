e83064fc0bc6965b3310a87ebb502dea54b74d1e
SashaPotemkin <alex.potjomkin@gmail.com>
2023-07-26 21:14:02 +0300

    Остановка сервисов

--- app/src/main/java/ru/potemkin/servicestest/MyForegroundService.kt
+++ app/src/main/java/ru/potemkin/servicestest/MyForegroundService.kt
@@ -33,6 +33,7 @@ class MyForegroundService : Service() {
                 delay(1000)
                 log("Timer $i")
             }
+            stopSelf()
         }
         return START_STICKY
     }
