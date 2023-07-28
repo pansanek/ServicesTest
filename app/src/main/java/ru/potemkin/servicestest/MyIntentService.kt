ab47fb5c9abdc4abf70ec7a3832a9aa0cca52fa2
SashaPotemkin <alex.potjomkin@gmail.com>
2023-07-28 15:45:38 +0300

    JobIntentService

--- app/src/main/java/ru/potemkin/servicestest/MyIntentService.kt
+++ app/src/main/java/ru/potemkin/servicestest/MyIntentService.kt
@@ -3,21 +3,14 @@ package ru.potemkin.servicestest
 import android.app.IntentService
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.IBinder
 import android.util.Log
 import androidx.core.app.NotificationCompat
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class MyIntentService : IntentService(NAME) {
-
 
     override fun onCreate() {
         super.onCreate()
@@ -27,18 +20,17 @@ class MyIntentService : IntentService(NAME) {
         startForeground(NOTIFICATION_ID, createNotification())
     }
 
+    override fun onHandleIntent(intent: Intent?) {
+        log("onHandleIntent")
+        for (i in 0 until 5) {
+            Thread.sleep(1000)
+            log("Timer $i")
+        }
+    }
 
     override fun onDestroy() {
         super.onDestroy()
         log("onDestroy")
-    }
-
-    override fun onHandleIntent(p0: Intent?) {
-        log("onStartCommand")
-            for (i in 0 until 100) {
-                Thread.sleep(1000)
-                log("Timer $i")
-            }
     }
 
     private fun log(message: String) {
@@ -68,7 +60,7 @@ class MyIntentService : IntentService(NAME) {
         private const val CHANNEL_ID = "channel_id"
         private const val CHANNEL_NAME = "channel_name"
         private const val NOTIFICATION_ID = 1
-        private const val NAME="MyIntentService"
+        private const val NAME = "MyIntentService"
 
         fun newIntent(context: Context): Intent {
             return Intent(context, MyIntentService::class.java)
--- app/src/main/java/ru/potemkin/servicestest/MyIntentService2.kt
+++ app/src/main/java/ru/potemkin/servicestest/MyIntentService2.kt
@@ -1,23 +1,16 @@
 package ru.potemkin.servicestest
-
 import android.app.IntentService
+
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.IBinder
 import android.util.Log
 import androidx.core.app.NotificationCompat
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class MyIntentService2 : IntentService(NAME) {
-
 
     override fun onCreate() {
         super.onCreate()
@@ -25,36 +18,32 @@ class MyIntentService2 : IntentService(NAME) {
         setIntentRedelivery(true)
     }
 
+    override fun onHandleIntent(intent: Intent?) {
+        log("onHandleIntent")
+        val page = intent?.getIntExtra(PAGE, 0) ?: 0
+        for (i in 0 until 5) {
+            Thread.sleep(1000)
+            log("Timer $i $page")
+        }
+    }
 
     override fun onDestroy() {
         super.onDestroy()
         log("onDestroy")
     }
 
-    override fun onHandleIntent(p0: Intent?) {
-        log("onStartCommand")
-        val page = p0?.getIntExtra(PAGE,0)?:0
-        for (i in 0 until 100) {
-            Thread.sleep(1000)
-            log("Timer $i")
-        }
-    }
-
     private fun log(message: String) {
-        Log.d("SERVICE_TAG", "MyIntentService: $message")
+        Log.d("SERVICE_TAG", "MyIntentService2: $message")
     }
-
-
 
     companion object {
 
+        private const val NAME = "MyIntentService2"
+        private const val PAGE = "page"
 
-        private const val NAME="MyIntentService"
-        private const val PAGE="page"
-
-        fun newIntent(context: Context,page:Int): Intent {
-            return Intent(context, MyIntentService::class.java).apply {
-                putExtra(PAGE,page)
+        fun newIntent(context: Context, page: Int): Intent {
+            return Intent(context, MyIntentService2::class.java).apply {
+                putExtra(PAGE, page)
             }
         }
     }
