2a431e9e0a588c9f3038e07f30eae8945d5d0b2f
SashaPotemkin <alex.potjomkin@gmail.com>
2023-07-28 14:57:55 +0300

    JobScheduler. Enqueue

--- app/src/main/java/ru/potemkin/servicestest/MainActivity.kt
+++ app/src/main/java/ru/potemkin/servicestest/MainActivity.kt
@@ -5,6 +5,7 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.job.JobInfo
 import android.app.job.JobScheduler
+import android.app.job.JobWorkItem
 import android.content.ComponentName
 import android.os.Build
 import android.os.Bundle
@@ -15,6 +16,8 @@ import ru.potemkin.servicestest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+
+    private var page = 0
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -34,18 +37,23 @@ class MainActivity : AppCompatActivity() {
         }
         binding.intentService.setOnClickListener {
             ContextCompat.startForegroundService(
-                this,MyIntentService.newIntent(this)
+                this, MyIntentService.newIntent(this)
             )
         }
         binding.jobScheduler.setOnClickListener {
-            val componentName = ComponentName(this,MyJobService::class.java)
-            val jobInfo = JobInfo.Builder(MyJobService.JOB_ID,componentName)
+            val componentName = ComponentName(this, MyJobService::class.java)
+
+            val jobInfo = JobInfo.Builder(MyJobService.JOB_ID, componentName)
                 .setRequiresCharging(true)
                 .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
                 .build()
-            val jobScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
-            jobScheduler.schedule(jobInfo)
-        }
 
+            val jobScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val intent = MyJobService.newIntent(page++)
+                jobScheduler.enqueue(jobInfo, JobWorkItem(intent))
+            }
+        }
     }
 }
--- app/src/main/java/ru/potemkin/servicestest/MyJobService.kt
+++ app/src/main/java/ru/potemkin/servicestest/MyJobService.kt
@@ -5,14 +5,16 @@ import android.app.job.JobParameters
 import android.app.job.JobService
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
 import android.os.Message
+import android.os.PersistableBundle
 import android.util.Log
 import kotlinx.coroutines.*
 
 class MyJobService : JobService() {
 
-    private val coroutineScopre = CoroutineScope(Dispatchers.Main)
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreate() {
         super.onCreate()
@@ -21,18 +23,27 @@ class MyJobService : JobService() {
 
     override fun onDestroy() {
         super.onDestroy()
-        coroutineScopre.cancel()
+        coroutineScope.cancel()
         log("OnDestroy")
     }
 
     override fun onStartJob(p0: JobParameters?): Boolean {
         log("OnStartCommand")
-        coroutineScopre.launch {
-            for (i in 0 until 100) {
-                delay(1000)
-                log("Timer $i")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            coroutineScope.launch {
+                var workItem = p0?.dequeueWork()
+                while (workItem != null) {
+                    val page = workItem.intent.getIntExtra(PAGE, 0)
+                        for (i in 0 until 5) {
+                            delay(1000)
+                            log("Timer $i,$page")
+                        }
+                        p0?.completeWork(workItem)
+                        workItem = p0?.dequeueWork()
+                    }
+
+                    jobFinished(p0, false)
             }
-            jobFinished(p0, true)
         }
         return true
     }
@@ -49,5 +60,12 @@ class MyJobService : JobService() {
     companion object {
 
         const val JOB_ID = 111
+        private const val PAGE = "page"
+
+        fun newIntent(page: Int): Intent {
+            return Intent().apply {
+                putExtra(PAGE, page)
+            }
+        }
     }
 }
